e5e8770904fb4b8f7ce4f7a64ee8425d7f29e104
farrelleoin93 <farrelleoin93@gmail.com>
2021-04-22 23:14:18 +0000

    add content and functionality to 404.html

--- app.py
+++ app.py
@@ -244,6 +244,13 @@ def filter():
                             category_name=category_name)
 
 
+# 404 ERROR
+@app.errorhandler(404)
+def error_404(error):
+    return render_template('errors/404.html', error=error), 404
+
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
