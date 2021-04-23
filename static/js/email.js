cd7e8922bc74b28a9aed84cb9da1013f11ebf6ab
farrelleoin93 <farrelleoin93@gmail.com>
2021-04-23 16:52:40 +0000

    add functionality to newsletter signup

---
+++ static/js/email.js
@@ -0,0 +1,28 @@
+// Code to email the completed form was found at https://www.emailjs.com/docs/
+
+function sendMail(contactForm) {
+    emailjs
+        .send("gmail", "seed_shelf", {
+            "from_email": contactForm.emailaddress.value,
+        })
+        .then(
+            function () {
+                swal({
+                    title: "Thanks for signing up!",
+                    text: "You'll now start recieving our weekly newsletter",
+                    icon: 'success',
+                    button: 'Done',
+                })
+            },
+            function (error) {
+                swal({
+                    title: "Oops, it looks like something went wrong.",
+                    text: "Please try again",
+                    icon: 'error',
+                    button: 'Ok',
+                })
+            }
+        );
+    document.getElementById("contactForm").reset();
+    return false;
+}
