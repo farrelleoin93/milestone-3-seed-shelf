1d5dad41b48f9d2f47efec20334954aa2340aa4a
farrelleoin93 <farrelleoin93@gmail.com>
2021-05-04 00:58:44 +0000

    put css and javascript files through formatter

--- static/js/email.js
+++ static/js/email.js
@@ -3,26 +3,27 @@
 function sendMail(contactForm) {
     emailjs
         .send("gmail", "seed_shelf", {
-            "from_email": contactForm.emailaddress.value,
+            from_email: contactForm.emailaddress.value,
         })
         .then(
             function () {
                 swal({
                     title: "Thanks for signing up!",
                     text: "You'll now start recieving our weekly newsletter",
-                    icon: 'success',
-                    button: 'Done',
-                })
+                    icon: "success",
+                    button: "Done",
+                });
             },
             function (error) {
                 swal({
                     title: "Oops, it looks like something went wrong.",
                     text: "Please try again",
-                    icon: 'error',
-                    button: 'Ok',
-                })
+                    icon: "error",
+                    button: "Ok",
+                });
             }
         );
     document.getElementById("contactForm").reset();
     return false;
 }
+
